beac2633b0cb163a6f23a1115ce5f15fe042224d
Katie Hansen <48529509+katiewrennhansen@users.noreply.github.com>
2019-08-12 18:01:25 -0400

    organize js

--- index.js
+++ index.js
@@ -1,32 +1,57 @@
-//MENU FILL-IN ON SCROLL
+//DOM VARIABLES
 const nav = document.querySelector('nav');
-const collapseContent = document.getElementById('navbar');
+const button = document.querySelector('button');
+const navbar = document.getElementById("navbar");
+const homePage = document.getElementById("home-page");
+const main = document.querySelector('main');
+const overlay = document.querySelectorAll('.overlay');
 
-document.addEventListener('scroll', function(){
-    nav.classList.add('background-color');
-    collapseContent.classList.add('background-color');
-    collapseContent.classList.add('transition');
-});
+
+
+
+//MENU FILL-IN ON SCROLL
+function scrollBehavior(){
+    document.addEventListener('scroll', function(){
+        nav.classList.add('background-color');
+        navbar.classList.add('background-color', 'transition');
+    });
+}
 
 
 
 //TOGGLE NAV BUTTON SHOW
-const button = document.querySelector('button');
-const navbar = document.getElementById("navbar")
-
-button.addEventListener('click', function(){
-    navbar.classList.toggle('show-nav');
-  });
+function navToggle(){
+    button.addEventListener('click', function(){
+        navbar.classList.toggle('show-nav');
+      });
+    main.addEventListener('click', function(){
+        navbar.classList.remove('show-nav');
+    });
+    homePage.addEventListener('click', function(){
+        navbar.classList.remove('show-nav');
+    });
+}
 
 
 
 
 //TOGGLE PROJECT IMAGE OVERLAY
-const overlay = document.querySelectorAll('.overlay');
-
-overlay.forEach(function(img){
-    img.addEventListener('click', function(){
-        this.classList.toggle('show');
+function overlayClick(){
+    overlay.forEach(function(img){
+        img.addEventListener('click', function(){
+            this.classList.toggle('show');
+        }); 
     }); 
-});
+}
 
+
+
+//CALL ALL FUNCTIONS
+function callAllFuntions(){
+    scrollBehavior();
+    navToggle();
+    overlayClick();
+}
+
+
+callAllFuntions();
