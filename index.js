6bdd534f0e8968af800f961ee04206e4440123b7
Katie Hansen <48529509+katiewrennhansen@users.noreply.github.com>
2019-08-13 19:15:11 -0400

    add styles, add nav js and animations

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 //DOM VARIABLES
 const nav = document.querySelector('nav');
+const logo = document.querySelector('.logo');
 const button = document.querySelector('button');
 const navbar = document.getElementById("navbar");
 const homePage = document.getElementById("home-page");
@@ -10,13 +11,21 @@ const overlay = document.querySelectorAll('.overlay');
 
 
 //MENU FILL-IN ON SCROLL
-function scrollBehavior(){
-    document.addEventListener('scroll', function(){
-        nav.classList.add('background-color');
-        navbar.classList.add('background-color', 'transition');
+function scrollBehavior() {
+    window.addEventListener('scroll', function(){
+        if (window.pageYOffset > 10){
+            nav.classList.add('background-color');
+            navbar.classList.add('transition', 'background-color');
+            logo.classList.add('up');
+        } else {
+            nav.classList.remove('background-color');
+            navbar.classList.remove('transition', 'background-color');
+            logo.classList.remove('up');
+        }
     });
 }
 
+ 
 
 
 //TOGGLE NAV BUTTON SHOW
