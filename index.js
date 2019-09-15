165cb322e8b9392a2a57f087be649a982f1dec19
Katie Hansen <48529509+katiewrennhansen@users.noreply.github.com>
2019-09-15 18:23:31 -0400

    update navbar

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 //DOM VARIABLES
-const nav = document.querySelector('nav');
+const nav = document.getElementById('nav');
 const logo = document.querySelector('.logo');
 const button = document.querySelector('button');
 const navbar = document.getElementById("navbar");
@@ -8,57 +8,45 @@ const content = document.getElementById("line");
 const main = document.querySelector('main');
 const projects = document.querySelectorAll('.single-proj');
 const overlay = document.querySelectorAll('.overlay');
+const collapse = document.querySelector('.collapse-content');
+const navLinks = document.querySelectorAll('.nav-link');
 
 
 
 //MENU FILL-IN ON SCROLL
-function scrollBehavior() {
-    window.addEventListener('scroll', function(){
-        if (window.pageYOffset > 10){
-            nav.classList.add('background-color');
-            navbar.classList.add('transition', 'background-color');
-            logo.classList.add('up');
-        } else {
-            nav.classList.remove('background-color');
-            navbar.classList.remove('transition', 'background-color');
-            logo.classList.remove('up');
-        }
-    });
+function scrollBehavior(){
+    if (window.pageYOffset > 10){
+        nav.classList.add('background-color');
+        navbar.classList.add('transition', 'background-color');
+        logo.classList.add('up');
+    } else {
+        nav.classList.remove('background-color');
+        navbar.classList.remove('transition', 'background-color');
+        logo.classList.remove('up');
+    }
 }
+
 
 function homePageFade(){
     $(document).ready(function() {
-        // $('h1.hidden').fadeIn(1800).removeClass('hidden');
-        // $('p.hidden').fadeIn(1800).removeClass('hidden');
         $('div.hidden').fadeIn(1800).removeClass('hidden');
         $('nav.hidden').fadeIn(1800).removeClass('hidden');
     });
 }
- 
 
 
-//TOGGLE NAV BUTTON SHOW
-function navToggle(){
-    button.addEventListener('click', function(){
-        navbar.classList.toggle('show-nav');
-      });
-    main.addEventListener('click', function(){
-        navbar.classList.remove('show-nav');
-    });
-    homePage.addEventListener('click', function(){
-        navbar.classList.remove('show-nav');
-    });
+//TOGGLE NAV 
+
+function toggleNav(){
+    collapse.classList.toggle('toggle');
+    button.classList.toggle('cross');
+    nav.classList.toggle('nav-background');
 }
 
-function navFocus(){
-    button.addEventListener('focus', function(){
-        navbar.classList.toggle('show-nav');
-      });
-    button.addEventListener('blur', function(){
-        navbar.classList.remove('show-nav');
-    });
+function removeNav(){
+    collapse.classList.remove('toggle');
+    button.classList.remove('cross');
 }
-
 
 
 
@@ -68,7 +56,7 @@ function overlayClick(){
         img.addEventListener('click', function(){
             this.classList.toggle('show');
         });
-    }); 
+    });
 }
 
 function overlayFocus(){
@@ -86,17 +74,25 @@ function overlayFocus(){
 }
 
 
+//ADD EVENT LISTENERS
+window.addEventListener('scroll', scrollBehavior);
+button.addEventListener('click', toggleNav);
+main.addEventListener('click', removeNav);
+homePage.addEventListener('click', removeNav);
+//open nav with tab
+button.addEventListener('focus', toggleNav);
+button.addEventListener('blur', removeNav);
+
+navLinks.forEach(link => {
+    link.addEventListener('click', removeNav);
+});
 
 
 //CALL ALL FUNCTIONS
 function callAllFuntions(){
     homePageFade();
-    scrollBehavior();
-    navToggle();
-    navFocus();
     overlayClick();
     overlayFocus();
-    iconLinkFocus();
 }
 
 
